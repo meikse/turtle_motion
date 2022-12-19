700611b333948c8bb9462f2fb7b5d4be4aebba49
meikse <meik_seedorf@web.de>
2022-12-20 00:06:25 +0100

    created ros2 publisher

--- src/position.py
+++ src/position.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python3
 
 from pyautogui import position 
-from time import sleep
 
-def main():
-    try:
-        while True:
+import rclpy
+from geometry_msgs.msg import Point
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = rclpy.create_node('mouse_locator')
+    publisher = node.create_publisher(Point, 'location/mouse', 1)
+
+    msg = Point()
+
+    def timer_callback():
+        try:
             x, y = position()
-            print (x,y, flush=True)
-            sleep(.1)
-    except KeyboardInterrupt:
-        pass
+            msg.x = float(x)
+            msg.y = float(y)
+            msg.z = 0.
+            publisher.publish(msg)
+            node.get_logger().info("x: {},y: {}".format(x,y))
+        except:
+            node.get_logger().info("mouse position not obtainable.")
+
+    period = .1
+    timer = node.create_timer(period, timer_callback)
+
+    rclpy.spin(node)
+
+    node.destroy_timer(timer)
+    node.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
     main()
-
