5784b724edb3071daab876f5fa5477e9d8b1f114
meikse <meik_seedorf@web.de>
2022-12-20 18:53:31 +0100

    prepared for transform file

--- src/control.cpp
+++ src/control.cpp
@@ -1,41 +1,3 @@
-/* #include <functional> */
-/* #include <memory> */
-
-/* #include <rclcpp/rclcpp.hpp> */
-/* #include <geometry_msgs/msg/point.hpp> */
-
-/* #include "std_msgs/msg/string.hpp" */
-
-/* using point = geometry_msgs::msg::Point; */
-/* using mytype = std_msgs::msg::String; */
-/* using std::placeholders::_1; */
-
-/* class ControlTurtle : public rclcpp::Node */ 
-/* { */
-/*   public: */
-/*     ControlTurtle() : */ 
-/*     Node("turtle_controller") */
-/*     { */
-/*       mouse_subs_ = this->create_subscription<mytype>( */
-/*       "location/mouse", 10, std::bind(&ControlTurtle::mouse_callback, this, _1)); */
-/*     } */
-/*   private: */
-/*     void mouse_callback(mytype & msg) const { */
-/*       RCLCPP_INFO(this->get_logger(), "here!"); */
-/*       /1* RCLCPP_INFO(this->get_logger(), "I heard: %lf", msg.x); *1/ */
-/*     }; */
-/*     rclcpp::Subscription<mytype>::SharedPtr mouse_subs_; */
-/* }; */
-
-
-/* int main(int argc, char** argv){ */
-/* { */
-/*   rclcpp::init(argc, argv); */
-/*   rclcpp::spin(std::make_shared<ControlTurtle>()); */
-/*   rclcpp::shutdown(); */
-/*   return 0; */
-/* } */
-
 #include <functional>
 #include <memory>
 
@@ -47,21 +9,24 @@ using std::placeholders::_1;
 
 class ControlTurtle : public rclcpp::Node
 {
-public:
+  rclcpp::Subscription<point>::SharedPtr mouse_subs_;
+  void mouse_callback(const point & msg) const;
+  void window_callback(const point & msg) const;
+  public:
   ControlTurtle()
-  : Node("turtle_controller")
+    : Node("turtle_controller")
   {
     mouse_subs_ = this->create_subscription<point>(
-      "location/mouse", 10, std::bind(&ControlTurtle::topic_callback, this, _1));
+        "mouse/location",
+        10, 
+        std::bind(&ControlTurtle::mouse_callback, this, _1));
   }
-
-private:
-  void topic_callback(const point & msg) const
-  {
-    RCLCPP_INFO(this->get_logger(), "here.");
-  }
-  rclcpp::Subscription<point>::SharedPtr mouse_subs_;
 };
+
+void ControlTurtle::mouse_callback(const point & msg) const
+{
+  RCLCPP_INFO(this->get_logger(), "here.");
+}
 
 int main(int argc, char * argv[])
 {
