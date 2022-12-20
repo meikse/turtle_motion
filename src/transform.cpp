b828fa8b6bb12456d068892af1eb01c3552f7ab7
meikse <meik_seedorf@web.de>
2022-12-20 21:00:36 +0100

    first version

--- src/transform.cpp
+++ src/transform.cpp
@@ -1,5 +1,6 @@
 #include <functional>
 #include <memory>
+#include <chrono>
 #include <vector>
 
 #include "rclcpp/rclcpp.hpp"
@@ -13,20 +14,51 @@ class Transform : public rclcpp::Node
   rclcpp::Subscription<point>::SharedPtr mouse_subs_;
   rclcpp::Subscription<point>::SharedPtr wind_loc_subs_;
   rclcpp::Subscription<point>::SharedPtr wind_dim_subs_;
-  std::vector<double> mouse, window, dimension;
+
+  rclcpp::Publisher<point>::SharedPtr center_pub_;
+  rclcpp::Publisher<point>::SharedPtr distance_pub_;
+
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  std::vector<double> mouse = {0., 0.};     // import to pass value
+  std::vector<double> dimension = {0., 0.}; // if not, segmentation error
+  std::vector<double> window = {0., 0.};    // rclcpp needs time to init
+
   void mouse_callback(const point & msg){
     this->mouse = {msg.x, msg.y};
-    RCLCPP_INFO(this->get_logger(), "mouse: %.6f", this->mouse[0]);
+    /* RCLCPP_INFO(this->get_logger(), "mouse: %.6f", this->mouse[0]); */
   }
+
   void window_loc_callback(const point & msg){
     this->window= {msg.x, msg.y};
-    RCLCPP_INFO(this->get_logger(), "window: %.6f", this->window[0]);
+    /* RCLCPP_INFO(this->get_logger(), "window: %.6f", this->window[0]); */
   }
+
   void window_dim_callback(const point & msg){
     this->dimension= {msg.x, msg.y};
-    RCLCPP_INFO(this->get_logger(), "dimension: %.6f", this->dimension[0]);
+    /* RCLCPP_INFO(this->get_logger(), "dimension: %.6f", this->dimension[0]); */
   }
+
+  void timer_callback(){
+
+    auto center = point();  // origin of turtle coord system (center of window)
+    center.x = this->window[0] + this->dimension[0] * .5;
+    center.y = this->window[1] + this->dimension[1] * .5;
+    center.z = 0.;
+    RCLCPP_INFO(this->get_logger(), "center: %.3f, %.3f", center.x, center.y);
+    center_pub_->publish(center);
+
+    auto distance = point(); // rel distance between mouse and turtle origin
+    distance.x = this->mouse[0] - center.x;
+    distance.y = (this->mouse[1] - center.y) * -1; // *-1 =>rotate origin frame
+    distance.z = 0.;                               // image frame -> real frame
+    RCLCPP_INFO(this->get_logger(),
+        "distan: %.3f, %.3f", distance.x, distance.y);
+    distance_pub_->publish(distance);
+  }
+
   public:
+
   Transform() : Node("transformation_node")
   {
     mouse_subs_ = this->create_subscription<point>(
@@ -43,6 +75,18 @@ class Transform : public rclcpp::Node
         "window/dimension",
         10, 
         std::bind(&Transform::window_dim_callback, this, _1));
+
+    distance_pub_ = this->create_publisher<point>(
+        "mouse/distance", 
+        10);
+
+    center_pub_ = this->create_publisher<point>(
+        "window/center", 
+        10);
+
+    timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(1000),
+        std::bind(&Transform::timer_callback, this));
   }
 };
 
